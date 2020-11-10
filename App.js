0a2ea4595a1846ea08f9cdf7776a9b5bbad2376e
Orla Connelly <g00356556@gmit.ie>
2020-11-10 20:59:29 +0000

    bottom nav

--- App.js
+++ App.js
@@ -1,74 +1,23 @@
 import  React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+
 import { createDrawerNavigator } from '@react-navigation/drawer';
-
-import Icon from 'react-native-vector-icons/ionicons';
-
-import ProfileScreen from './screens/ProfileScreen';
-import TasksScreen from './screens/TasksScreen';
+import MainTabsScreen from './screens/MainTabsScreen';
+// import { Ionicons } from '@expo/vector-icons';
 
 
-const TasksStack = createStackNavigator();
-const ProfileStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-const TasksStackScreen = ({navigation}) => (
-<TasksStack.Navigator screenOptions={{
-        headerStyle: {
-            backgroundColor: '#009387',
-          },
-          headerTintColor: '#fff',
-          headerTintStyle: {
-            fontWeight: 'bold'
-          }
-          }}>
-        <TasksStack.Screen name="TasksScreen" component={TasksScreen} 
-        options={{
-       title: 'Overview',
-       headerLeft: () => (
-        <Icon.Button name="ios-menu" size={25}
-        backgroundColor="#009387" onPress={() => navigation.openDrawer
-        ()}></Icon.Button>
-       )
-        }} />
-        
-      </TasksStack.Navigator>
-);
 
-const ProfileStackScreen = ({navigation}) => (
-  <ProfileStack.Navigator screenOptions={{
-          headerStyle: {
-              backgroundColor: '#009387',
-            },
-            headerTintColor: '#fff',
-            headerTintStyle: {
-              fontWeight: 'bold'
-            }
-            }}>
-
-          <ProfileStack.Screen name="ProfileScreen" component={ProfileScreen}
-           options={{
-         headerLeft: () => (
-           <Icon.Button name="ios-menu" size={25}
-           backgroundColor="#009387" onPress={() => navigation.openDrawer
-           ()}></Icon.Button>
-         )
-  
-          }} />
-          
-        </ProfileStack.Navigator>
-  );
   
 
-const App =() =>{
+const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Tasks">
-        <Drawer.Screen name="Tasks" component={TasksStackScreen} />
-        <Drawer.Screen name="Profile" component={ProfileStackScreen} />
+        <Drawer.Screen name="Tasks" component={MainTabsScreen} />
+        {/* <Drawer.Screen name="Profile" component={ProfileStackScreen} /> */}
       </Drawer.Navigator>
-      {/*  */}
     </NavigationContainer>
   );
 }
--- screens/MainTabsScreen.js
+++ screens/MainTabsScreen.js
@@ -1,33 +1,118 @@
 import  React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Icon from 'react-native-vector-icons'
-import { View, Text, Button, } from 'react-native';
+// import Icon from 'react-native-vector-icons';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Ionicons } from '@expo/vector-icons';
+// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import ProfileScreen from './ProfileScreen';
+import TasksScreen from './TasksScreen';
+import FilesScreen from './FilesScreen';
+import FormsScreen from './FormsScreen';
 
 
 
+const TasksStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MainTabsScreen = () => (
+<Tab.Navigator
+      initialRouteName="Tasks"
+      activeColor="#e91e63"
+      style={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Tasks"
+        component={TasksStackScreen}
+        options={{
+          tabBarLabel: 'Tasks',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="ios-home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Files"
+        component={FilesScreen}
+        options={{
+          tabBarLabel: 'Files',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="ios-bell" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileStackScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="ios-account" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Forms"
+        component={FormsScreen}
+        options={{
+          tabBarLabel: 'Forms',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="ios-account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
 
 
+);
 
+export default MainTabsScreen;
 
-const HomeScreen = ({navigation}) => {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Task Screen</Text>
-        <Button
-        title="Go to Profile screen"
-        onPress={() => navigation.navigate("ProfileScreen")}
-        />
-      </View>
+const TasksStackScreen = ({navigation}) => (
+    <TasksStack.Navigator screenOptions={{
+            headerStyle: {
+                backgroundColor: '#009387',
+              },
+              headerTintColor: '#fff',
+              headerTintStyle: {
+                fontWeight: 'bold'
+              }
+              }}>
+            <TasksStack.Screen name="TasksScreen" component={TasksScreen} 
+            options={{
+           title: 'Overview',
+           headerLeft: () => (
+            <Ionicons.Button name="ios-menu" size={25}
+            backgroundColor="#009387" onPress={() => navigation.openDrawer
+            ()}></Ionicons.Button>
+           )
+            }} />
+            
+          </TasksStack.Navigator>
     );
-  };
-  
-  const DetailScreen = () => {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Detail Screen</Text>
-      </View>
-    );
-  }
-  
+    
+    const ProfileStackScreen = ({navigation}) => (
+      <ProfileStack.Navigator screenOptions={{
+              headerStyle: {
+                  backgroundColor: '#009387',
+                },
+                headerTintColor: '#fff',
+                headerTintStyle: {
+                  fontWeight: 'bold'
+                }
+                }}>
+    
+              <ProfileStack.Screen name="ProfileScreen" component={ProfileScreen}
+               options={{
+             headerLeft: () => (
+               <Ionicons.Button name="ios-menu" size={25}
+               backgroundColor="#009387" onPress={() => navigation.openDrawer
+               ()}></Ionicons.Button>
+             )
+      
+              }} />
+              
+            </ProfileStack.Navigator>
+      );
